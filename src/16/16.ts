df97f6432c62866dccefead9a2e928f4699b023c
Max Pimm <maxmil@gmail.com>
2022-12-27 08:18:31 +0100

    Day 16 - Part 2

--- src/16/16.ts
+++ src/16/16.ts
@@ -1,36 +1,84 @@
 import fs from 'fs';
 import { deflateSync } from 'zlib';
 
-type Valve = { name: string; rate: number; neighbours: string[] };
+type Valve = { name: string; rate: number; neighbours: Map<string, number> };
+
+const valves = new Map<string, Valve>();
+
+function parseValves() {
+  fs.readFileSync('src/16/input.txt', 'utf-8')
+    .split('\n')
+    .map((line) => line.match(/Valve ([A-Z]{2}) has flow rate=(\d+)[a-z;\s]+(.*)/) as string[])
+    .map(([_, name, rate, neighbors]) => {
+      valves.set(name, { name, rate: Number(rate), neighbours: new Map(neighbors.split(', ').map((name) => [name, 1])) });
+    });
+}
+
+function removeZeroRateNonStartNodes() {
+  const removeValves = Array.from(valves.values()).filter((v) => v.rate === 0 && v.name !== 'AA');
+  removeValves.forEach((removeValve) => {
+    valves.forEach((valve) => {
+      if (valve.name === removeValve.name) valves.delete(valve.name);
+      const distToRemoved = valve.neighbours.get(removeValve.name);
+      if (distToRemoved) {
+        removeValve.neighbours.forEach((distance, neighbor) => {
+          if (neighbor !== valve.name)
+            valve.neighbours.set(neighbor, Math.min(valve.neighbours.get(neighbor) ?? Number.POSITIVE_INFINITY, distToRemoved + distance));
+        });
+      }
+      valve.neighbours.delete(removeValve.name);
+    });
+  });
+}
 
 const pressureCache = new Map<string, number>();
 
-function maxPressure(currValve: string, minutesRemaining: number, openedValves: Set<string>): number {
-  if (minutesRemaining < 2) return 0;
-  const key = `${currValve},${minutesRemaining},${Array.from(openedValves).join('')}`;
-  if (pressureCache.has(key)) return pressureCache.get(key)!;
+function isOpen(valveStates: number, name: string): boolean {
+  const keys = Array.from(valves.keys());
+  const index = keys.indexOf(name);
+  const open = 1 << (keys.length - index);
+  return (valveStates & open) !== 0;
+}
+
+function openValve(valveStates: number, name: string): number {
+  const keys = Array.from(valves.keys());
+  const index = keys.indexOf(name);
+  const open = 1 << (keys.length - index);
+  return valveStates | open;
+}
+
+function maxPressure(currValve: string, minutesRemaining: number, valveStates: number, elephant: boolean = false): number {
+  if (minutesRemaining === 0) {
+    if (elephant) return maxPressure('AA', 26, valveStates, false);
+    else return 0;
+  }
+
+  const key = `${currValve},${minutesRemaining},${valveStates},${elephant}`;
+  const cached = pressureCache.get(key);
+  if (cached != undefined) return cached;
 
   const valve = valves.get(currValve)!;
-  let pressureFromOpening = 0;
-  if (!openedValves.has(currValve) && valve.rate > 0) {
-    const newOpenedValves = new Set(openedValves);
-    newOpenedValves.add(currValve);
-    const newPressureFromOpenning = valve.rate * (minutesRemaining - 1);
-    pressureFromOpening = newPressureFromOpenning + maxPressure(currValve, minutesRemaining - 1, newOpenedValves);
+
+  let pressureByOpenning = 0;
+  if (!isOpen(valveStates, currValve) && valve.rate > 0) {
+    const pressureFromValve = valve.rate * (minutesRemaining - 1);
+    pressureByOpenning = pressureFromValve + maxPressure(currValve, minutesRemaining - 1, openValve(valveStates, currValve), elephant);
   }
-  
-  const pressure = valve.neighbours
-    .map((neighbour) => maxPressure(neighbour, minutesRemaining - 1, openedValves))
-    .reduce((max, pressure) => Math.max(max, pressure), pressureFromOpening);
+
+  const pressureByMoving = Array.from(valve.neighbours.entries())
+    .filter(([_, distance]) => distance < minutesRemaining - 1)
+    .map(([neighbour, distance]) => maxPressure(neighbour, minutesRemaining - distance, valveStates, elephant));
+
+  const pressureByElephant = pressureByMoving.length == 0 && elephant ? maxPressure('AA', 26, valveStates, false) : 0;
+
+  const pressure = [pressureByOpenning, ...pressureByMoving, pressureByElephant].reduce((max, pressure) => Math.max(max, pressure), 0);
 
   pressureCache.set(key, pressure);
   return pressure;
 }
-const input = fs.readFileSync('src/16/input.txt', 'utf-8').split('\n');
-const valves = new Map(
-  input
-    .map((line) => line.match(/Valve ([A-Z]{2}) has flow rate=(\d+)[a-z;\s]+(.*)/) as string[])
-    .map(([_, name, rate, neighbors]) => [name, { name, rate: Number(rate), neighbours: neighbors.split(', ') }]),
-);
 
-console.log(`Part 1: ${maxPressure('AA', 30, new Set())}`);
+parseValves();
+removeZeroRateNonStartNodes();
+
+console.log(`Part 1: ${maxPressure('AA', 30, 0)}`);
+console.log(`Part 2: ${maxPressure('AA', 26, 0, true)}`);
